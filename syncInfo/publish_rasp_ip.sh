4e243c869edbbd5fff7fdf083d4ea9f48128e412
wxyz <hyhjwzx@126.com>
2016-03-19 21:25:21 +0800

    每次提交ip地址的时候，带上主机名和时间

--- syncInfo/publish_rasp_ip.sh
+++ syncInfo/publish_rasp_ip.sh
@@ -12,13 +12,18 @@ oldIp=`cat $ipFile`
 getIpUrl='http://members.3322.org/dyndns/getip'
 
 currIp=`curl -s $getIpUrl`
+hostname=`cat /etc/hostname`
+currTime=`date +%F_%T`
 #echo $currIp
 
 if [ "$oldIp" = "$currIp" ]; then
 	echo "ip not change"
 else
 	echo "new ip is $currIp"
-	echo $currIp > $ipFile
+	
+	git pull
+	
+	echo "$hostname $currTime $currIp" >> $ipFile
 	
 	git add $ipFile
 	git commit -m "${machine} ip address was changed."
